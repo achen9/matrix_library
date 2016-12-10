17bf1b56c34dcc3b9401f7f33a11c72ce73f4743
Alex Chen <alexac9@u.washington.edu>
2016-12-09 19:38:00 -0800

    Add complex class constructor and unit tests

--- src/complex.rs
+++ src/complex.rs
@@ -19,6 +19,141 @@
 //! Alex Z. Chen - 12/05/2016
 //! alexac9@uw.edu
 //!
-mod complex {
+// Derivations
+#[derive(Clone)]
+#[derive(Copy)]
 
+// Data definition
+pub struct Complex {
+  real: f64,
+  imaginary: f64,
 }
+
+// Constructors
+pub fn complex(r: f64, i: f64) -> Complex {
+  Complex {real: r, imaginary: i}
+}
+
+// Methods
+impl Complex {
+  // Getters
+  pub fn re(&self) -> f64 { self.real }
+  pub fn im(&self) -> f64 { self.imaginary }
+
+  // Setters
+  pub fn set_re(&mut self, r: f64) { self.real = r; }
+  pub fn set_im(&mut self, i: f64) { self.imaginary = i; }
+}
+ /*
+  // Reduce method
+  pub fn reduce(&self) -> Complex {
+    let mut abs_num = self.num().abs();
+    let mut abs_den = self.den().abs();
+    let mut gcd = 1;
+    // Modified Euclidean algorithm to find greatest common divisor (GCD)
+    while (0 != abs_num) && (0 != abs_den) {
+      if abs_num > abs_den {
+        gcd = abs_den;
+        abs_num %= abs_den;
+      } else {
+        gcd = abs_num;
+        abs_den %= abs_num;
+      }
+    }
+    // Eliminate double negatives or move negative to real
+    if 0 > self.den() {
+        gcd *= -1;
+      }
+    Complex {real: self.num()/gcd, imag: self.den()/gcd}
+  }
+
+  // Raising fraction to a power method
+  pub fn pow(&self, exp: isize) -> Complex {
+    
+    if 0 > exp {
+      // Negative exponents flip the fraction
+      let exp_u32 = -exp as u32;
+      let f = Complex {real: self.den().pow(exp_u32), imag: self.num().pow(exp_u32)};
+      f.reduce()
+    } else {
+      let exp_u32 = exp as u32;
+      let f = Complex {real: self.num().pow(exp_u32), imag: self.den().pow(exp_u32)};
+      f.reduce()
+    }
+  }
+}
+
+// Arithmetic operations & operator overload
+impl ::std::ops::Add for Complex {
+  type Output = Complex;
+  fn add(self, other: Complex) -> Complex {
+    let f = Complex {real: self.num() * other.den() + self.den() * other.num(), imag: self.den() * other.den()};
+    f.reduce()
+  }
+}
+impl ::std::ops::Sub for Complex {
+  type Output = Complex;
+  fn sub(self, other: Complex) -> Complex {
+    let f = Complex {real: self.num() * other.den() - self.den() * other.num(), imag: self.den() * other.den()};
+    f.reduce()
+  }
+}
+impl ::std::ops::Mul for Complex {
+  type Output = Complex;
+  fn mul(self, other:Complex) -> Complex {
+    let f = Complex {real: self.num() * other.num(), imag: self.den() * other.den()};
+    f.reduce()
+  }
+}
+impl ::std::ops::Div for Complex {
+  type Output = Complex;
+  fn div(self, other:Complex) -> Complex {
+    if 0 == other.num() {
+      panic!("Attempted to divide by zero.");
+    }
+    let f = Complex {real: self.num() * other.den(), imag: self.den() * other.num()};
+    f.reduce()
+  }
+}
+
+// Unary operator overload
+impl ::std::ops::Neg for Complex {
+  type Output = Complex;
+  fn neg(self) -> Complex {
+    let f = Complex {real: -self.num(), imag: self.den()};
+    f.reduce()
+  }
+}
+
+// Comparison operator overloads
+use ::std::cmp::Ordering;
+impl ::std::cmp::PartialEq for Complex {
+  fn eq(&self, other: &Complex) -> bool {
+    self.num() * other.den() == self.den() * other.num()
+  }
+}
+impl ::std::cmp::Eq for Complex {}
+impl ::std::cmp::PartialOrd for Complex {
+  fn partial_cmp(&self, other: &Complex) -> Option<Ordering> {
+    Some(self.cmp(other))
+  }
+}
+impl Ord for Complex {
+  fn cmp(&self, other: &Complex) -> Ordering {
+    if (0 > self.den()) ||(0 > other.den()) {
+      // Need to add negative sign to num() and f.num() because 
+      // negative in imag causes comparison to be flipped
+      (-self.num() * other.den()).cmp(&(-other.num() * self.den()))
+    } else {
+      (self.num() * other.den()).cmp(&(other.num() * self.den()))
+    }
+  }
+}
+
+// Print formatting
+impl ::std::fmt::Display for Complex {
+  fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+    write!(f, "{}/{}", self.num(), self.den())
+  }
+}
+*/
---
+++ tests/complex_unit_tests.rs
@@ -0,0 +1,195 @@
+//! NAME
+//!  complex_unit_tests.rs
+//!
+//! DESCRIPTION
+//!  Unit test file for complex class methods
+//!
+//! PARAMETERS
+//!  See specific method
+//!
+//! RETURN
+//!  See specific method
+//! 
+//! EXAMPLE
+//!  See specific method
+//!
+//! NOTES
+//!  Make sure the Cargo.toml file is available to compile the code
+//!
+//! Alex Z. Chen - 12/09/2016
+//! alexac9@uw.edu
+//!
+extern crate matrix_lib;
+// A tolerance constant is needed when comparing doubles. Due to rounding
+// errors, doing a straight comparison of doubles may cause erroneous 
+// results. Checking the two doubles are within some tolerance is a better
+// method to check for equality.
+const TOLERANCE: f64 = 0.00001;
+
+// Constructor Test: Test if 2/3 can be assigned to a complex 
+#[test]
+fn constructor_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c: Complex = complex(2.6,3.8);
+  println!("C real: {:.5} imag: {:.5}",c.re(),c.im());
+  assert!(TOLERANCE > (c.re() - 2.6).abs());
+  assert!(TOLERANCE > (c.im() - 3.8).abs());
+}
+// Copy Constructor Test: Test if complex -3/4 can be copied to another variable
+#[test]
+fn copy_constructor_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(-3.95,4.12);
+  let mut c2: Complex = c1.clone();
+  println!("C1 real: {:.5} imag: {:.5}",c1.re(),c1.im());
+  assert!(TOLERANCE > (c2.re() + 3.95).abs());
+  assert!(TOLERANCE > (c2.im() - 4.12).abs());
+  c2.set_re(5.23);
+  assert!(TOLERANCE > (c2.re() - 5.23).abs());
+  assert!(TOLERANCE > (c1.re() + 3.95).abs());
+}/*
+// Reduce method Test: Test if complex 8/-32 can be reduced to -1/4
+#[test]
+fn reduce_method_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(8,-32);
+  let c2: Complex = c1.reduce();
+  assert!(-1 == c2.re());
+  assert!(4 == c2.im());
+  assert!(8 == c1.re());
+  assert!(-32 == c1.im());
+}
+// Arithmetic Operation Overload Test 1: Test if 3/4 + 2/-3 = 1/12
+#[test]
+fn addition_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(3,4);
+  let c2: Complex = complex(2,-3);
+  let c = c1 + c2;
+  assert!(1 == c.re());
+  assert!(12 == c.im());
+}
+// Arithmetic Operation Overload Test 2: Test if 2/4 - 2/3 = -1/6
+#[test]
+fn subtraction_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(2,4);
+  let c2: Complex = complex(2,3);
+  let c = c1 - c2;
+  assert!(-1 == c.re());
+  assert!(6 == c.im());
+}
+// Arithmetic Operation Overload Test 3: Test if 4/-5 * 3/4 = -3/5
+#[test]
+fn multiplication_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(4,-5);
+  let c2: Complex = complex(3,4);
+  let c = c1 * c2;
+  assert!(-3 == c.re());
+  assert!(5 == c.im());
+}
+// Arithmetic Operation Overload Test 4: Test if (-5/7) / (-6/7) = 5/6
+#[test]
+fn division_test1() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(-5,7);
+  let c2: Complex = complex(-6,7);
+  let c = c1 / c2;
+  assert!(-5 == c1.re()); // Check c1 still exists and can be used
+  assert!(-6 == c2.re()); // Check c2 still exists and can be used
+  assert!(5 == c.re());
+  assert!(6 == c.im());
+}
+// Arithmetic Operation Overload Test 4: Test if (-5/7) / (0/2) = panic
+#[test]
+#[should_panic]
+fn division_test2() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(-5,7);
+  let c2: Complex = complex(0,2);
+  let c = c1 / c2;
+  assert!(true); // If assertion passes, something went wrong
+}
+// Unary Negate Operator Overload Test : Test if -(-5/-7) = -5/7
+#[test]
+fn negate_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(-5,-7);
+  let c = -c1;
+  assert!(-5 == c.re()); 
+  assert!(7 == c.im());
+  assert!(-5 == c1.re()); // Check c1 still exists and can be used
+}
+// Comparison Operator Overload Test 1: Test if 3/-4 == -12/16 => true
+#[test]
+fn equals_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(3,-4);
+  let c2: Complex = complex(-12,16);
+  assert!(c1 == c2);
+}
+// Comparison Operator Overload Test 2: Test if 3/-4 != -11/16 => true
+#[test]
+fn not_equals_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(3,-4);
+  let c2: Complex = complex(-11,16);
+  assert!(c1 != c2);
+}
+// Comparison Operator Overload Test 3: Test if 3/-4 < 1/2 => true
+#[test]
+fn less_than_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(3,-4);
+  let c2: Complex = complex(1,2);
+  assert!(c1 < c2);
+}
+// Comparison Operator Overload Test 4: Test if 1/3 > 3/-4 => true
+#[test]
+fn greater_than_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(3,-4);
+  let c2: Complex = complex(1,3);
+  assert!(c2 > c1);
+}
+// Comparison Operator Overload Test 5: Test if 1/3 <= 2/6 => true
+#[test]
+fn less_than_equals_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(1,3);
+  let c2: Complex = complex(2,6);
+  assert!(c1 <= c2);
+  let c3: Complex = complex(-5,1);
+  assert!(c3 <= c1);
+}
+// Comparison Operator Overload Test 6: Test if 1/-8 >= -4/32 => true
+#[test]
+fn greater_than_equals_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(1,-8);
+  let c2: Complex = complex(-4,32);
+  assert!(c1 >= c2);
+  let c3: Complex = complex(1,2);
+  assert!(c3 >= c1);
+}
+// Raising to a Power Test: Test if (2/-4)^3 = -1/8
+#[test]
+fn power_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c1: Complex = complex(2,-4);
+  let c2: Complex = c1.pow(3);
+  assert!(-1 == c2.re());
+  assert!(8 == c2.im());
+  let c3: Complex = c1.pow(-3);
+  assert!(-8 == c3.re());
+  assert!(1 == c3.im());
+}
+// Printing complex to terminal
+#[test]
+fn print_test() {
+  use matrix_lib::complex::{Complex, complex};
+  let c: Complex = complex(2,-4);
+  println!("Complex c is {}. c reduced is {}.", c, c.reduce())
+}
+*/
