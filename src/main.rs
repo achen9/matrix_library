a8acf23e91cb0b9899ee68a5009a808c91fbc196
Alex Chen <alexac9@u.washington.edu>
2016-12-14 21:10:07 -0800

    Add tests in main.rs for matrix constructor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate matrix_lib;
+
 fn main() {
-    println!("Hello, world! Float: {:.5} Integer: {}", 2.654, 4);
+  use matrix_lib::matrix::{Matrix, matrix};
+  use matrix_lib::fraction::Fraction;
+  use matrix_lib::complex::Complex;
+  let m_f: Matrix<Fraction> = matrix(3,3);
+  let m_c: Matrix<Complex> = matrix(3,3);
+  let m_b: Matrix<bool> = matrix(3,3);
+  println!("Matrix m_f is: {}Matrix m_c is: {}Matrix m_b is: {}", m_f, m_c, m_b);
 }
